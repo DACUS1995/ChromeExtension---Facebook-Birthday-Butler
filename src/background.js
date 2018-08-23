9f86d530056d9b15082bd753c4530cc15af83238
DACUS1995 <studormarian@yahoo.co.uk>
2018-08-23 22:42:17 +0300

    popup page seup

--- src/background.js
+++ src/background.js
@@ -1,32 +1,40 @@
 'use strict';
 
-chrome.runtime.onInstalled.addListener(function() {
+// const HOSTNAME_TARAGET = 'developer.chrome.com'
+const HOSTNAME_TARAGET = 'www2.gogoanime.in';
+
+
+chrome.runtime.onInstalled.addListener(function() 
+{
 	console.log("The Buttler is on duty!");
 
-	chrome.storage.sync.set({color: '#3aa757'}, function() {
-		console.log('The color is green.');
+	chrome.storage.sync.set({birthday: ''}, function() {
+		console.log("Birthday is set.");
 	});
 
-	const declarativeContentRule = {
+	const declarativeContentRule = 
+	{
 		conditions: [new chrome.declarativeContent.PageStateMatcher({
-			pageUrl: {hostEquals: "*.gogoanime.in/*"},
+			pageUrl: {hostEquals: HOSTNAME_TARAGET},
 		})],
 		actions: [new chrome.declarativeContent.ShowPageAction()]
 	};
 
 	/**
 	 * Define when the extension is usable.
-	 * (Added rules are saved across browser restarts and when loading 
+	 * (Added rules are saved across browser restarts and thus when loading 
 	 * the extension we need to remove them)
 	 * 
-	 * By using the page action API we can take action without inject content scripts
+	 * By using the page action API we can take action without automatically injecting content scripts
 	 */
-	chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+	chrome.declarativeContent.onPageChanged.removeRules(undefined, function() 
+	{
 		chrome.declarativeContent.onPageChanged.addRules([declarativeContentRule]);
 	});
 });
 
-chrome.runtime.onSuspend.addListener(function() {
+chrome.runtime.onSuspend.addListener(function() 
+{
 	console.log("Unloading.");
 	chrome.browserAction.setBadgeText({text: ""});
 })
