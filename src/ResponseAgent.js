1450f591d05d99c3f35c3cf7ae4175545b632cfd
DACUS1995 <studormarian@yahoo.co.uk>
2018-08-29 01:11:11 +0300

    added message handler to the agent instance

--- src/ResponseAgent.js
+++ src/ResponseAgent.js
@@ -1,31 +1,38 @@
 "use strict"
 
+import MessageHandler from "./ResponseAgentModules/MessageHandler.js"
+
 class ResponseAgent
 {
     constructor()
     {
-
+        this._incomingMessageHandler = null;
     }
 
     init()
     {
+        this._setup()
+    }
 
+    _setup()
+    {
+        this._incomingMessageHandler = new MessageHandler(this);
     }
 }
 
 (new ResponseAgent).init();
 
-// Listen for runtime messages
-chrome.runtime.onMessage.addListener(function(request, sender, response){
-    // To check if the message was sent from a content script check for the existance of sender.tab
-    if(!sender.tab)
-    {
-        console.log(request.message);
-        response({message: "received"});
-    }
-});
+// // Listen for runtime messages
+// chrome.runtime.onMessage.addListener(function(request, sender, response){
+//     // To check if the message was sent from a content script check for the existance of sender.tab
+//     if(!sender.tab)
+//     {
+//         console.log(request.message);
+//         response({message: "received"});
+//     }
+// });
 
-//Send message to extension runtime environment
-chrome.runtime.sendMessage({message: "hello"}, function(response) {
-    console.log(response.message);
-});
+// //Send message to extension runtime environment
+// chrome.runtime.sendMessage({message: "hello"}, function(response) {
+//     console.log(response.message);
+// });
